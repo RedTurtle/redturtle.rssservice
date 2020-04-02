59bd405dc3f6b87f7a5eea7c1af276c065c7cb2a
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-04-02 11:59:50 +0200

    fix implementation and first tests

--- src/redturtle/rssservice/service.py
+++ src/redturtle/rssservice/service.py
@@ -1,8 +1,14 @@
 from plone.restapi.services import Service, _no_content_marker
+from redturtle.rssservice import _
+from zope.i18n import translate
+
 import requests
 
 
 class GetRSSFeedService(Service):
+    """
+    Proxy route
+    """
 
     content_type = "application/rss+xml"
 
@@ -12,28 +18,33 @@ class GetRSSFeedService(Service):
 
         self.check_permission()
         content = self.reply()
-        if content is not _no_content_marker:
+        if content.get('error', {}):
             self.request.response.setHeader("Content-Type", self.content_type)
-            return content
+            return content['error'].get('message')
+        self.request.response.setHeader("Content-Type", self.content_type)
+        return content.get('data', '')
 
     def reply(self):
-
-        form = self.request.form
-        if not form.get("feed", None):
-            self.request.response.setStatus(500)
+        feed = self.request.form.get('feed', '')
+        if not feed:
+            self.request.response.setStatus(400)
             return dict(
                 error=dict(
-                    type="InternalServerError",
-                    message=self.error_message
+                    type="BadRequest",
+                    message=translate(
+                        _(
+                            'missing_feed_parameter',
+                            default='Missing required parameter: feed',
+                        ),
+                        context=self.request,
+                    ),
                 )
             )
-        response = requests.get(form.get("feed"))
+        response = requests.get(feed)
         if response.status_code != 200:
-            self.request.response.setStatus(500)
+            self.request.response.setStatus(response.status_code)
+            message = response.text or response.reason
             return dict(
-                error=dict(
-                    type="InternalServerError",
-                    message=self.error_message
-                )
+                error=dict(type="InternalServerError", message=message)
             )
-        return response._content
+        return {'data': response.text}
